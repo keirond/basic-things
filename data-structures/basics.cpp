025a5ba74ebe07af8439a9d00e1b347096fce0c2
Bao. Dang <dang.hbaokody@gmail.com>
2024-10-25 02:19:31 +0000

    temp push & switch device

---
+++ data-structures/basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+std::string __string = "string";
+
+int __array [] = { 1, 2, 3, 4, 5 };
+
+std::list<int> __list = { 1, 2, 3, 4, 5 };
+
+std::vector<int> __vector = { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < 10; i++) {
+	std::cout << i << std:endl;
+}
